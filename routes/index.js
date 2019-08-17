cfa3be8da3a2fa34cc1e3c0b943e46e0034e2081
keemsisi <keemsisi@gmail.com>
2019-08-17 07:00:53 -0700

    another commit change with message update

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ router.use(express.static(path.join(__dirname, 'public/pgs-app'))); // the pgs-a
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.redirect('/index.html');
+  // res.redirect('/index.html');
+  res.send("<h3>SERVER IS ALIVE AT THE MOMENT and SERVING : </h3>"+Date.now())
 });
 
 module.exports = router;
